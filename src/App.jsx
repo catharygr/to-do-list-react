3f46e8ab5381684ac3958d71f4ba8144ea2bdafe
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-30 17:11:12 +0100

    Aplicación terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import TodoList from "./componentes/TodoList";
-import AddTodo from "./componentes/AddTodo";
 
 function App() {
   return (
     <div>
       <TodoList />
-      <AddTodo />
     </div>
   );
 }
--- src/componentes/TodoList.jsx
+++ src/componentes/TodoList.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
-// Function component TodoList
+
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
-  // useEffect para guardar los todos en el localStorage
+
   useEffect(() => {
     const storedTodos = localStorage.getItem("todos");
     if (storedTodos) {
       setTodos(JSON.parse(storedTodos));
     }
   }, []);
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
@@ -17,7 +18,13 @@ function TodoList() {
   const addTodo = (e) => {
     e.preventDefault();
     if (newTodo.trim() !== "") {
-      setTodos([...todos, newTodo]);
+      const newTodoObj = {
+        id: Date.now(),
+        text: newTodo,
+        completed: false,
+      };
+
+      setTodos([...todos, newTodoObj]);
       setNewTodo("");
     }
   };
@@ -29,25 +36,30 @@ function TodoList() {
   };
 
   return (
-    <form onSubmit={addTodo} className="todo-container">
+    <div className="todo-container">
       <h2 className="todo-list">Todo List</h2>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
+          <li key={todo.id}>
+            {todo.text}
             <button className="btn-delete" onClick={() => deleteTodo(index)}>
               Delete
             </button>
           </li>
         ))}
       </ul>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button>Add Todo</button>
-    </form>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          className="add-todo-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit" className="add-todo-button">
+          Add Todo
+        </button>
+      </form>
+    </div>
   );
 }
 
