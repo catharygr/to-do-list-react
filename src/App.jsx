a64b980fedd2b907c00cd42afd81d548e9a74955
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-03 20:12:55 +0100

    Trabajando en el css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import TodoList from "./componentes/TodoList";
+import React from "react";
+
+import TodoList from "./TodoList";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  function handleAddItem(label) {
+    const newItem = {
+      label,
+      id: Math.random(),
+    };
+
+    const nextItems = [...items, newItem];
+    setItems(nextItems);
+  }
+
   return (
-    <div>
-      <TodoList />
+    <div className="wrapper">
+      <div className="list-wrapper">
+        <ol className="shopping-list">
+          {items.map(({ id, label }) => (
+            <li key={id}>{label}</li>
+          ))}
+        </ol>
+      </div>
+      <TodoList handleAddItem={handleAddItem} />
     </div>
   );
 }
